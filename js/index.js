e2425d79a50ac3f34e49e17b5851c9db02697622
Evaldas Latoskinas <E.latoskinas@student.tudelft.nl>
2019-03-26 00:12:22 +0100

    Highlight anchor link on scroll

--- js/index.js
+++ js/index.js
@@ -5,10 +5,23 @@ var menus = [],
 var scrollId = 0;
 
 $(window).scroll(function() {
-    // Get container scroll position
-    var fromTop = $(this).scrollTop() + scrollPositions[0];
+    var scrollPos = $(this).scrollTop() + scrollPositions[0];
 
-    console.log(fromTop);
+    var oldScrollId = scrollId;
+
+    if (scrollPos < scrollPositions[scrollId] && scrollId > 0) {
+        scrollId--;
+    }
+    else {
+        while (scrollId < scrollItems.length-1 && scrollPos >= scrollPositions[scrollId+1]) {
+            scrollId++;
+        }
+    }
+
+    if (oldScrollId != scrollId) {
+        menus[oldScrollId].dataset.active = false;
+        menus[scrollId].dataset.active = true;
+    }
 });
 
 $(document).ready(function() {
@@ -19,7 +32,21 @@ $(document).ready(function() {
         if (item.length) { return item; }
     })
 
+    var scrollPos = $(window).scrollTop();
+
     for (var i = 0; i < scrollItems.length; ++i) {
-        scrollPositions.push(scrollItems[i][0].offsetTop);
+        // Get absolute height of i-th scrollItem
+        // -20 difference is used since first value is at position 20
+        var scrollValue = scrollItems[i][0].offsetTop - 20;
+
+        // Add value to scrollPositions
+        scrollPositions.push(scrollValue);
+
+        // ScrollPos exceeds scrollValue, set section as active
+        if (scrollPos >= scrollValue) {
+            scrollId = i;
+        }
     }
+
+    menus[scrollId].dataset.active = true;
 });
